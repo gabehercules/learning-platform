7d29afd552fda3facac597bf87ba4d28d3dedc1f
Gabriel <gabrielsyze@gmail.com>
2022-06-25 10:17:16 -0300

    updated and added features

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4r15myx0gg201z18sb8en3c/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache(),
 })
